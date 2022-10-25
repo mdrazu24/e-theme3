474ecfb38d4c585271a9075dc34f082db7480e26
mdrazu2543 <alif.pab120na.12@gmail.com>
2022-10-25 18:44:16 +0600

    changes

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/sass/app.scss",
             ],
-            refresh: [
-                ...refreshPaths,
-            ],
+            refresh: [...refreshPaths],
         }),
     ],
 });
